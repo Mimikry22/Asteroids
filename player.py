f9b3423db160b72c1e605d519f2c9edc2640f381
Mimikry22 <kcm22@gmx.de>
2025-01-20 17:28:35 +0100

    Added bullet cooldown

--- player.py
+++ player.py
@@ -3,9 +3,11 @@ from constants import *
 from bullet import *
 
 class Player(CircleShape):
+    
     def __init__(self, x, y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -24,7 +26,7 @@ class Player(CircleShape):
     
     def update(self,dt):
         keys = pygame.key.get_pressed()
-
+        self.shoot_timer -= dt
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -32,15 +34,18 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_w]:
-            self.move(dt)
+            self.move(dt)  
         if keys[pygame.K_SPACE]:
             self.shoot()
-        
+                
 
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
         bullet = Shot(self.position.x, self.position.y)
         bullet.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
