f551509b7049f12bb601c63aba49bde6aa48e0c1
Mimikry22 <kcm22@gmx.de>
2025-01-20 13:59:39 +0100

    Added FPS

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
         screen.fill('black')
         pygame.display.flip()
+        dt_time = clock.tick(60)
+        dt = dt_time/1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
